68c05362adebd7e879500a8eaef5f263e65710c7
aleksandrs12 <90208363+aleksandrs12@users.noreply.github.com>
2021-12-08 14:54:32 +0200

    Add files via upload

---
+++ for.py
@@ -0,0 +1,32 @@
+import pygame
+
+
+
+pygame.init()
+win = pygame.display.set_mode((800, 700))
+image = pygame.image.load('map.png')
+
+
+run = True
+
+btn = [[0, 0, 150, 120, 'a1'], [0, 120, 150, 120, 'b1']]
+def a1():
+    win.blit(pygame.image.load('a1.png'), (150, 100))
+
+
+def b1():
+    win.blit(pygame.image.load('b1.png'), (150, 100))
+
+while run:
+    win.blit(image, (0, 0))
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+    mpos = pygame.mouse.get_pos()
+    for n in btn:
+        if mpos[0] > n[0] and mpos[0] < n[0] + n[2] and mpos[1] > n[1] and mpos[1] < n[1] + n[3] and pygame.mouse.get_pressed()[0]:
+            eval(n[4]+'()')
+
+    pygame.display.update()
+
+pygame.quit
